729466812521b28fab3469830f5d364495f7a511
EdenPark <edengoforit@gmail.com>
2023-02-19 18:52:13 +1300

    Route added for the home

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import "./App.css";
 import Home from "./routes/home/home.component";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
