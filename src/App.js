6bec910ae5c647575685f1e731e76c6de18823e3
EdenPark <edengoforit@gmail.com>
2023-02-19 20:40:57 +1300

    navigation component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 }
