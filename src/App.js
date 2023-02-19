188efd244491edc17a62b63b2ac25e9b7b43563e
EdenPark <edengoforit@gmail.com>
2023-02-19 18:28:32 +1300

    created and moved to home page

--- src/App.js
+++ src/App.js
@@ -1,43 +1,8 @@
 import "./App.css";
-import "./categories.styles.scss";
-import CategoryItem from "./components/category-item/category-item.component";
+import Home from "./routes/home/home.component";
 
 function App() {
-  const categories = [
-    {
-      id: 1,
-      title: "HATS",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "JACKETS",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "SNEAKERS",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "WOMENS",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "MENS",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
-  return (
-    <div className="categories-container">
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
