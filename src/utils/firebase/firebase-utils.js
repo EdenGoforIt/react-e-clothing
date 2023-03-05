608ff6c19906b6a7eb96bf4e68f9f1b5ec611c14
EdenPark <edengoforit@gmail.com>
2023-03-05 13:47:55 +1300

    firebase setup

--- src/utils/firebase/firebase-utils.js
+++ src/utils/firebase/firebase-utils.js
@@ -1,5 +1,10 @@
 import { initializeApp } from 'firebase/app'
-import { getAuth, signInWithRedirect, signInWithPopup } from 'firebase/auth'
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -13,3 +18,12 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig)
+
+const provider = new GoogleAuthProvider()
+
+provider.setCustomParameters({
+  prompt: 'select_account',
+})
+
+export const auth = getAuth()
+export const SignInWithGooglePopup = () => signInWithPopup(auth, provider)
